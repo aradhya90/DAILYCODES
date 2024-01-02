a8ddc44c1d74b849a97bcc59ac352d8e8fe3eb3b
aradhya90 <143214777+aradhya90@users.noreply.github.com>
2024-01-02 18:15:41 +0530

    Add files via upload

---
+++ c++ coding/p1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    /*int len,brea,peri;
+    //float volcyl;;
+    cin>>len>>brea;
+    //volcyl=3.14*rad1*rad1*hgt;
+    peri=2*(len*brea);
+    cout<<"perimeter of rectangle is :"<<peri<<endl;
+    */
+int n1,n2,n3,n4;
+//cout<<"input the first two numbers:"<<endl;
+cin>>n1>>n2;
+cout<<" the first two numbers:"<<endl;
+
+
+
+   return 0;
+}
---
+++ c++ coding/p2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+   int main() 4
+{
+    float km,m;
+    cin>>km;
+    cout<<" the kilometer is:"<<km<<endl;
+   
+   m=km* 0.6213712;
+   cout<<"the meter  is:"<<m<<endl;
+
+
+   return 0;
+}
